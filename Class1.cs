7dc306c79228a04798e3a8bf09f4f790260ef55d
nico <nico.barreiro@hotmail.com>
2015-06-01 12:08:39 +0200

    Codigo Metodos 02

--- Class1.cs
+++ Class1.cs
@@ -21,22 +21,48 @@ namespace Ejercicio_4_4_02
         private string colorOjos02;
         private string colorPelo02;
 
-        
+
         public void PonerLentillas02(string colorLentillas)
         {
-            
+            if (comprobarColor02(colorLentillas))
+                this.colorOjos02 = colorLentillas;
+        }
+
+        private static bool comprobarColor02(string color)
+        {
+            return color.Equals("azul") || color.Equals("marron") || color.Equals("verde");
         }
 
 
         public void Engordar02(int cantidad)
         {
-     
+            int nuevo_peso = this.peso02 + cantidad;
+
+            if ((nuevo_peso) > 180)
+            {
+                Console.WriteLine("Cantidad incorrecta");
+            }
+            else
+            {
+                Console.WriteLine("Incrementando del peso en {0} kg", cantidad);
+                this.peso02 = nuevo_peso;
+            }
         }
 
 
         public void Adelgazar02(double cantidad)
         {
-      
+            double nuevo_peso = this.peso02 - cantidad;
+
+            if ((nuevo_peso) < 40)
+            {
+                Console.WriteLine("Cantidad incorrecta");
+            }
+            else
+            {
+                Console.WriteLine("Reduciendo el peso en {0} kg", cantidad);
+                this.peso02 = (int)nuevo_peso;
+            }
         }
     }
 }
