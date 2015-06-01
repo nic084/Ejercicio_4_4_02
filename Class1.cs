001771cba297ade3082a4d4376fca6303f018f30
nico <nico.barreiro@hotmail.com>
2015-06-01 12:01:23 +0200

    Inicializacion02

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4_4_02
+{
+    public class Persona02
+    {
+        public Persona02(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.altura02 = altura;
+            this.colorOjos02 = colorOjos;
+            this.colorPelo02 = colorPelo;
+            this.peso02 = peso;
+        }
+
+        private int peso02;
+        private int altura02;
+        private string colorOjos02;
+        private string colorPelo02;
+
+        
+        public void PonerLentillas02(string colorLentillas)
+        {
+            
+        }
+
+
+        public void Engordar02(int cantidad)
+        {
+     
+        }
+
+
+        public void Adelgazar02(double cantidad)
+        {
+      
+        }
+    }
+}
